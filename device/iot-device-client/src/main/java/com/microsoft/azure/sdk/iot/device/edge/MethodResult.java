cc94d1c1e6fd7cafd7c015712e7e80a5b0b96fe8
timtay-microsoft <timtay@microsoft.com>
2018-08-06 11:10:59 -0700

    temp

--- device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/edge/MethodResult.java
+++ device/iot-device-client/src/main/java/com/microsoft/azure/sdk/iot/device/edge/MethodResult.java
@@ -43,20 +43,8 @@ public class MethodResult
     //    return this.payload;
     //}
 
-    public String getPayload()
+    public Object getPayload()
     {
-        // Codes_SRS_DIRECTMETHODRESULT_34_002: [This function shall return the saved status.]
-        if (this.payload instanceof String)
-        {
-            return (String) this.payload;
-        }
-        else if (this.payload instanceof byte[])
-        {
-            return new String((byte[]) this.payload);
-        }
-        else
-        {
-            return this.payload.toString();
-        }
+        return this.payload;
     }
 }
